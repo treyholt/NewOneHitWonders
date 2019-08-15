755f05e448b8a0ea396f6eea8fc2a573562e47a7
Trey Holterman <trey.holterman@healthiq.com>
2019-08-15 16:18:12 -0700

    Adjusted for token

--- script.py
+++ script.py
@@ -18,7 +18,6 @@ def show_tracks(tracks):
 if __name__ == '__main__':
     username = "1252330876"
     token = util.prompt_for_user_token(username,scope = 'user-library-modify',client_id='a5c2c927ceb242b2936166108355ac0e',client_secret='09557185c27b44a48e920986b097df4c',redirect_uri='http://localhost/')
-    print("WE Have a token: ", token)
 
     if token:
         sp = spotipy.Spotify(auth=token)
@@ -36,38 +35,5 @@ if __name__ == '__main__':
                     tracks = sp.next(tracks)
                     show_tracks(tracks)
     else:
-        print("Can't get token for", username)
+        print("Token has been invalidated")
 
-# #!/usr/bin/env python3
-# """
-# Code to find artists and songs that greatly outperform the average
-# content produced by said artist. 
-# """
-# __author__ = "Diego Baugh + Trey Holterman"
-# __version__ = "0.1.0"
-# __license__ = "MIT"
-
-# import spotipy
-# export SPOTIPY_CLIENT_ID = 'a5c2c927ceb242b2936166108355ac0e'
-# export SPOTIPY_CLIENT_SECRET = '09557185c27b44a48e920986b097df4c'
-# export SPOTIPY_REDIRECT_URI = 'http://localhost/'
-
-
-# def setupAuth():
-#     lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
-#     spotify = spotipy.Spotify()
-#     results = spotify.artist_top_tracks(lz_uri)
-#     for track in results['tracks'][:10]:
-#         print("track    : " + track['name'])
-#         print("audio    : " + track['preview_url'])
-#         print("cover art: " + track['album']['images'][0]['url'])
-#         print()
-
-# def main():
-#     """ Main entry point of the app """
-#     print("in main")
-#    # setupAuth()
-
-
-# if __name__ == "__main__":
-#     main()
